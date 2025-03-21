f6b8ed7a70a9104da145e89a52a69e9702587e7e
Alimnfl <mnaufallim234@gmail.com>
2025-03-21 07:46:11 +0700

    add: nav hooks added.

---
+++ hooks/useNav.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type NavProps = {
+  selected: {
+    nav: string;
+  };
+  setSelected: (data: object) => void;
+};
+
+const useNav = create<NavProps>()(
+  persist(
+    (set) => ({
+      selected: { nav: "" },
+      setSelected: (data) =>
+        set((state) => ({
+          selected: { ...state.selected, ...data },
+        })),
+    }),
+    {
+      name: "nav-storage",
+    }
+  )
+);
+
+export default useNav;
